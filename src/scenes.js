e1172dd2cd53e867ccbdd59d2ac9d6716099dcc9
stra <str.tnk18@icloud.com>
2026-09-25 08:49:13 +0900

    debug

--- src/scenes.js
+++ src/scenes.js
@@ -135,8 +135,5 @@ export class TitleScene extends FNFScene {
     }
 
     onCreate() {
-        this.input.setDefaultCursor("none");
-        this.scene.launch("DebugScene");
-        this.drawText(630,360,"DebugTeXt012,.;:@[]");
     }
 }
