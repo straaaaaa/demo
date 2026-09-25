be17817486b6cb2e841152e221f9db79d2375ba1
stra <str.tnk18@icloud.com>
2026-09-25 10:55:34 +0900

    a

--- src/scenes.js
+++ src/scenes.js
@@ -7,7 +7,7 @@ export class BootScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.atlasXML("phantommuff","/assets/font/phantommuff.png","/assets/font/phantommuff.xml");
+        this.load.atlasXML("phantommuff","./assets/font/phantommuff.png","./assets/font/phantommuff.xml");
     }
 
     create() {
